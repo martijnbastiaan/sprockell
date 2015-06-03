ef8b18d9ddab45d7f9a609b5e3bd64efef0f0ffc
Jan Kuper <j.kuper@utwente.nl>
2015-06-03 22:16:52 +0200

    Update TypesEtc.hs

--- src/TypesEtc.hs
+++ src/TypesEtc.hs
@@ -42,33 +42,31 @@ data Operator = Add  | Sub | Mul  | Div | Mod
               deriving (Eq,Show,Read)
 
 data Instruction = 
-          -- Compute opCode r0 r1 r2: go to "alu",
-          -- do "opCode" on regs r0, r1, and put result in reg r2
-          Compute Operator Reg Reg Reg
-                                         
+          Compute Operator Reg Reg Reg       -- Compute opCode r0 r1 r2: go to "alu",
+                                             --       do "opCode" on regs r0, r1, and put result in reg r2
         | Const Value Reg
 
         | Branch Reg Target
         | Jump Target
 
-        | Load MemAddr Reg                -- Load (Addr a) r : from "memory a" to "regbank r"
-                                          -- Load (Imm  v) r : put "Int v" in "regbank r"
-        | Store Reg MemAddr               -- Store (Addr r) a: from "regbank r" to "memory a"
-                                          -- Store (Imm  v) r: put "Int v" in "memory r"
-        | Push Reg                        -- push a value on the stack
-        | Pop Reg                         -- pop a value from the stack
+        | Load MemAddr Reg                   -- Load (Addr a) r : from "memory a" to "regbank r"
+                                             -- Load (Imm  v) r : put "Int v" in "regbank r"
+        | Store Reg MemAddr                  -- Store (Addr r) a: from "regbank r" to "memory a"
+                                             -- Store (Imm  v) r: put "Int v" in "memory r"
+        | Push Reg                           -- push a value on the stack
+        | Pop Reg                            -- pop a value from the stack
 
-        | TestAndSet MemAddr    -- Test address for zero and sets it to one if it is. Returns 1 on success,
-                    -- and 0 on failure. This is an atomic operation; it might therefore be
-                    -- use to implement locks or synchronisation.
-        | Read MemAddr    -- Read from input and put value in regA
-        | Receive Reg        -- Read on its way
-        | Write Reg MemAddr    -- Write content of regA to output
+        | TestAndSet MemAddr                 -- Test address for zero and sets it to one if it is. Returns 1 on success,
+                                             -- and 0 on failure. This is an atomic operation; it might therefore be
+                                             -- use to implement locks or synchronisation.
+        | Read MemAddr                       -- Read from input and put value in regA
+        | Receive Reg                        -- Read on its way
+        | Write Reg MemAddr                  -- Write content of regA to output
 
-        | EndProg        -- end of program, deactivates Sprockell. If all sprockells are at
-                    -- this instruction, the simulation will halt.
-        | Nop            -- "No operation"
-        | Debug String
+        | EndProg                            -- end of program, deactivates Sprockell. If all sprockells are at
+                                             -- this instruction, the simulation will halt.
+        | Nop                                -- "No operation".
+        | Debug String                       -- No real instruction, for debug purposes.
         deriving (Eq,Show,Read)
 
 -- ==========================================================================================================
