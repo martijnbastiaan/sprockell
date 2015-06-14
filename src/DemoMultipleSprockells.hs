cef1a86e413fd0adcd13a2fd63aab491874966cd
Leon Schoorl <l.m.schoorl@student.utwente.nl>
2015-06-15 00:25:10 +0200

    Fix DemoMultipleSprockells.hs
    All the Sprockells were jumping straight to the end without entering the loop.
    Also added a debug function to show it is working.

--- src/DemoMultipleSprockells.hs
+++ src/DemoMultipleSprockells.hs
@@ -11,11 +11,11 @@ import Sprockell.System
 
 prog :: [Instruction]
 prog = [
-           Branch SPID (Rel 3) 
-         , Const 10 RegC
-         , Write RegC (Addr 1) -- Sprockell 1 must jump to end
-         , Write RegC (Addr 2) -- Sprockell 2 must jump to end
-         , Jump (Abs 10)       -- Sprockell 0 jumps to end
+           Branch SPID (Rel 5)
+         , Const 11 RegC
+         , Write RegC (Addr 1) -- Sprockell 1 must jump to second end
+         , Write RegC (Addr 2) -- Sprockell 2 must jump to second end
+         , Jump (Abs 10)       -- Sprockell 0 jumps to first end
          -- BEGIN: loop
          , Read (Deref SPID)
          , Receive RegA
@@ -23,9 +23,24 @@ prog = [
          , Branch RegB (Rel (-3))
          -- END: loop
          , Jump (Ind RegA)
+
+         -- 10:
+         , EndProg
+
+         -- 11: Sprockells 1 and 2 are sent here
          , EndProg
        ]
 
 
-main = run 3 prog >> putChar '\n'
+main = runDebug debugEndProg 3 prog >> putChar '\n'
 
+-- This debug function show a message when a Sprockell reaches an EndProg instruction.
+debugEndProg SysState{sprs=sprs,instrs=instrs} = concat $ map isHalting sprs
+    where
+        isHalting SprState{regbank=regs,halted=halted}
+            | not halted && instrs!pc == EndProg
+                = "EndProg on Sprockell " ++ show spid ++ " at addr " ++ show pc ++ "\n"
+            | otherwise = ""
+            where
+                pc   = regs ! PC
+                spid = regs ! SPID
